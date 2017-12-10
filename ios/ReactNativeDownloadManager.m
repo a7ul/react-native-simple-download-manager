6d0e446a1f626796a4e628a645b1bc20e35b4c8b
Atul R <atulanand94@gmail.com>
2017-12-10 18:25:08 +0530

    Ios Base modules

---
+++ ios/ReactNativeDownloadManager.m
@@ -0,0 +1,13 @@
+#import "ReactNativeDownloadManager.h"
+
+@implementation ReactNativeDownloadManager
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(download:(RCTResponseSenderBlock)callback)
+{
+ 
+}
+
+
+@end
