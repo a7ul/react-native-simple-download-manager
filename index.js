6acef7a3a9f04c741bd66658b6f36f95cfcb58b4
Atul R <atulanand94@gmail.com>
2017-12-11 13:59:01 +0530

    fix for js file

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-
 import {NativeModules} from 'react-native';
 
 const {ReactNativeDownloadManager} = NativeModules;
 
 const noop = () => {};
 
-const getRequestConfig = (url, config) => ({
+const getRequestConfig = (config, url) => Object.assign({}, {
   downloadTitle: 'File Download',
   downloadDescription: url,
   saveAsName: 'Downloaded File - ' + new Date(),
   allowedInRoaming: true,
   allowedInMetered: true,
-  showInDownloads: true,
-  ...config
-});
+  showInDownloads: true
+}, config);
 
 const download = (url = '', headers = {}, config = {}, onEnd = noop) => {
-  const downloadRequestConfig = getRequestConfig(config);
+  const downloadRequestConfig = getRequestConfig(config, url);
   ReactNativeDownloadManager.download(url, headers, downloadRequestConfig, onEnd);
 };
 
-const queueDownload = (url = '',  headers = {}, config = {}, onStart = noop) => {
-  const downloadRequestConfig = getRequestConfig(config);
+const queueDownload = (url = '', headers = {}, config = {}, onStart = noop) => {
+  const downloadRequestConfig = getRequestConfig(config, url);
   ReactNativeDownloadManager.queueDownload(url, headers, downloadRequestConfig, onStart);
 };
 
@@ -29,10 +27,9 @@ const attachOnCompleteListener = (downloadId = '', onComplete = noop) => {
   ReactNativeDownloadManager.attachOnCompleteListener(downloadId, onComplete);
 };
 
-const cancel = (downloadId = '', onCancel = noop) =>  {
+const cancel = (downloadId = '', onCancel = noop) => {
   ReactNativeDownloadManager.cancel(downloadId, onCancel);
 };
-
 
 const checkStatus = (downloadId = '', onStatus = noop) => {
   ReactNativeDownloadManager.checkStatus(downloadId, onStatus);
