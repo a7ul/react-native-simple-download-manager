dbb6a226bc4e3f05d8132b5b2ead9953827397a1
Atul R <atulanand94@gmail.com>
2017-12-12 15:35:02 +0530

    Promisify

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import {NativeModules} from 'react-native';
 
 const {ReactNativeDownloadManager} = NativeModules;
 
-const noop = () => {};
-
 const getRequestConfig = (config, url) => Object.assign({}, {
   downloadTitle: 'File Download',
   downloadDescription: url,
@@ -13,27 +11,56 @@ const getRequestConfig = (config, url) => Object.assign({}, {
   showInDownloads: true
 }, config);
 
-const download = (url = '', headers = {}, config = {}, onEnd = noop) => {
+const download = (url = '', headers = {}, config = {}) => {
   const downloadRequestConfig = getRequestConfig(config, url);
-  ReactNativeDownloadManager.download(url, headers, downloadRequestConfig, onEnd);
+  return new Promise((resolve, reject) => {
+    ReactNativeDownloadManager.download(url, headers, downloadRequestConfig, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(data);
+    });
+  });
 };
 
-const queueDownload = (url = '', headers = {}, config = {}, onStart = noop) => {
+const queueDownload = (url = '', headers = {}, config = {}) => {
   const downloadRequestConfig = getRequestConfig(config, url);
-  ReactNativeDownloadManager.queueDownload(url, headers, downloadRequestConfig, onStart);
+  return new Promise((resolve, reject) => {
+    ReactNativeDownloadManager.queueDownload(url, headers, downloadRequestConfig, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(data);
+    });
+  });
 };
 
-const attachOnCompleteListener = (downloadId = '', onComplete = noop) => {
-  ReactNativeDownloadManager.attachOnCompleteListener(downloadId, onComplete);
-};
+const attachOnCompleteListener = (downloadId = '') => new Promise((resolve, reject) => {
+  ReactNativeDownloadManager.attachOnCompleteListener(downloadId, (err, data) => {
+    if (err) {
+      return reject(err);
+    }
+    return resolve(data);
+  });
+});
 
-const cancel = (downloadId = '', onCancel = noop) => {
-  ReactNativeDownloadManager.cancel(downloadId, onCancel);
-};
+const cancel = (downloadId = '') => new Promise((resolve, reject) => {
+  ReactNativeDownloadManager.cancel(downloadId, (err, data) => {
+    if (err) {
+      return reject(err);
+    }
+    return resolve(data);
+  });
+});
 
-const checkStatus = (downloadId = '', onStatus = noop) => {
-  ReactNativeDownloadManager.checkStatus(downloadId, onStatus);
-};
+const checkStatus = (downloadId = '') => new Promise((resolve, reject) => {
+  ReactNativeDownloadManager.checkStatus(downloadId, (err, data) => {
+    if (err) {
+      return reject(err);
+    }
+    return resolve(data);
+  });
+});
 
 module.exports = {
   download,
